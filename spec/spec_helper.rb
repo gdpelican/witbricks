8d73802d40e910cf1182da07080c77d4c076d1d5
James Kiesel <james.kiesel@gmail.com>
2014-09-05 15:09:16 -0400

    Rspec upgrade

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,11 +5,12 @@ ENV['RAILS_ENV'] = 'test'
 require File.expand_path('../../config/environment', __FILE__)
 
 require 'shoulda/matchers'
+require 'rspec/rails'
 
 RSpec.configure do |config|
+  config.infer_spec_type_from_file_location!
   config.include FactoryGirl::Syntax::Methods
   config.filter_run_excluding off: true
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
