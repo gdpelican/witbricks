e4058bfe90cc645cb5745c4a9ad335aaee209b36
James Kiesel <james.kiesel@gmail.com>
2014-09-06 13:51:50 -0400

    Write failing protractor test (as a test WHAT!!?)

--- spec/javascripts/example_spec.js
+++ spec/javascripts/example_spec.js
@@ -1,6 +1,6 @@
 describe('Example Specifcation', function() {
-    it('should be able to find the rails test server', function(done) {
-        expect(true).toBeTruthy();
+    it('should be able to find the rails test server (I should fail!)', function(done) {
+        expect(false).toBeTruthy();
         done();
     })
 });
