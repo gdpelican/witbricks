9ace38e1975e7c243037869be808706b0cb742b4
James Kiesel <james.kiesel@gmail.com>
2014-09-06 15:41:28 -0400

    Set protractor test to pass

--- spec/javascripts/example_spec.js
+++ spec/javascripts/example_spec.js
@@ -1,6 +1,6 @@
 describe('Example Specifcation', function() {
     it('should be able to find the rails test server (I should fail!)', function(done) {
-        expect(false).toBeTruthy();
+        expect(true).toBeTruthy();
         done();
     })
 });
